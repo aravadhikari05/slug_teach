db5309af365dd55ac76a990b8205607d2fdfc67e
ashwinsm10 <ashwinsm10@gmail.com>
2024-02-25 17:21:23 -0800

    better

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -2,14 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:slug_teach/pages/colors.dart';
 import 'package:slug_teach/pages/home_page.dart';
 import 'package:slug_teach/pages/profilepage.dart';
+import 'package:slug_teach/pages/messages.dart';
 
-import 'messages.dart';
+class Tutor {
+  final String name;
+  final String subject;
+
+  Tutor({required this.name, required this.subject});
+}
 
 class SearchPage extends StatefulWidget {
   @override
   _SearchPageState createState() => _SearchPageState();
 }
+
 class _SearchPageState extends State<SearchPage> {
+  List<Tutor> tutors = [
+    Tutor(name: 'Travis Scott', subject: 'English'),
+    Tutor(name: 'Jennifer Lopez', subject: 'Science'),
+    Tutor(name: 'Tom Cruise', subject: 'Math'),
+  ];
+
+  List<Tutor> filteredTutors = [];
+
+  @override
+  void initState() {
+    filteredTutors = tutors;
+    super.initState();
+  }
+
+  void filterTutors(String query) {
+    //check for subjets
+    setState(() {
+      filteredTutors = tutors
+          .where((tutor) =>
+          tutor.subject.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +50,12 @@ class _SearchPageState extends State<SearchPage> {
         automaticallyImplyLeading: false,
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: (){Navigator.push(
+          onPressed: () {
+            Navigator.push(
               context,
-              MaterialPageRoute(
-                  builder: (context) => HomePage()));},
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          },
         ),
         title: const Text('Search'),
       ),
@@ -34,53 +67,52 @@ class _SearchPageState extends State<SearchPage> {
               icon: Icon(Icons.home),
               onPressed: () {
                 Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            HomePage()));
-
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
               },
             ),
             IconButton(
               icon: Icon(Icons.search),
-              onPressed: () {
-
-
-
-              },
+              onPressed: () {},
             ),
             IconButton(
               icon: Icon(Icons.chat),
               onPressed: () {
                 Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => Messages()));
-
+                  context,
+                  MaterialPageRoute(builder: (context) => Messages()),
+                );
               },
             ),
             IconButton(
               icon: Icon(Icons.account_circle),
               onPressed: () {
                 Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            ProfilePage()));
-
+                  context,
+                  MaterialPageRoute(builder: (context) => ProfilePage()),
+                );
               },
             ),
           ],
         ),
       ),
       body: Column(
-
         children: [
-
-          SearchBar(),
-          const Expanded(
-            child: Center(
-              child: Text('Search results will be displayed here!'),
+          SearchBar(onChanged: filterTutors),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredTutors.length,
+              itemBuilder: (context, index) {
+                final tutor = filteredTutors[index];
+                return ListTile(
+                  leading: CircleAvatar(
+                    child: Text(tutor.name[0]),
+                  ),
+                  title: Text(tutor.name),
+                  subtitle: Text('Subject: ${tutor.subject}'),
+                );
+              },
             ),
           ),
         ],
@@ -90,13 +122,18 @@ class _SearchPageState extends State<SearchPage> {
 }
 
 class SearchBar extends StatelessWidget {
+  final Function(String) onChanged;
+
+  const SearchBar({required this.onChanged});
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(16),
-      child: const TextField(
+      child: TextField(
+        onChanged: onChanged,
         decoration: InputDecoration(
-          labelText: 'Search',
+          labelText: 'Search by Subject',
           border: OutlineInputBorder(),
           prefixIcon: Icon(Icons.search),
         ),
@@ -104,4 +141,3 @@ class SearchBar extends StatelessWidget {
     );
   }
 }
-
